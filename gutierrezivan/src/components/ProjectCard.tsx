8148b1b37ccfc750d2f146ff47ce9cfd7963a28f
ivanharvard <ivangutierrez@college.harvard.edu>
2025-09-25 20:10:20 -0400

    project card

---
+++ gutierrezivan/src/components/ProjectCard.tsx
@@ -0,0 +1,26 @@
+import type { Project } from "../data/projects";
+
+export default function ProjectCard(
+    { title, blurb, liveUrl, codeUrl, tags }: Project
+) {
+    return (
+        <article className="card proj">
+            <h3>{title}</h3>
+            <p>{blurb}</p>
+            {/* Checks if tags exists */}
+            {tags?.length ? (
+                // If tags exists, render each one
+                <div className="tags">
+                    {tags.map((t) => (
+                        <span key={t} className="tag">{t}</span>
+                    ))}
+                </div>
+            ) : null}
+            {/* Hyperlinks */}
+            <div className="actions">
+                {liveUrl && <a className="btn" href={liveUrl} target="_blank" rel="noreferrer">Visit</a>}
+                {codeUrl && <a className="btn" href={codeUrl} target="_blank" rel="noreferrer">See Code</a>}
+            </div>
+        </article>
+    )
+}
